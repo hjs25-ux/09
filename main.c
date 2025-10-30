fb918dc1ec0a725e283f4e7de48c755df2ea82ab
hjs25-ux <hjs25@sookmyung.ac.kr>
2025-10-30 15:45:22 +0900

    02 수정

--- main.c
+++ main.c
@@ -8,15 +8,17 @@ int main(int argc, char *argv[])
   int sum;
   int grade[SIZE];
   
-  sum = 0;
-  for(i=0;i<5;i++)
+  sum=0;
+  for(i=0;i<SIZE;i++)
   {
-   printf("학생 성적을 입력하세요:");
-   scanf("%d",&grade[i]);
-   sum += grade[i];
+    printf("성적입력:");
+    scanf("%d",&grade[i]);
+    sum += grade[i];
 }
-  average = sum/SIZE;
-  printf("성적평균:%d\n", average); 
+
+  average=sum/SIZE;
+  printf("평균:%d\n", average); 
+  
   
   
   system("PAUSE");	
