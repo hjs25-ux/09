06a8f5b1ad452ca6852d10ffb557d09f63388655
hjs25-ux <hjs25@sookmyung.ac.kr>
2025-10-30 15:18:52 +0900

    01

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+  int score[5];
+  int i;
+  
+  score[0]=10;
+  score[1]=20;
+  score[2]=30;
+  score[3]=40;
+  score[4]=50;
+  
+  for (i=0;i<5;i++)
+  {
+      printf("score[%d]=%d\n",i,score[i]); 
+}
+  
+  system("PAUSE");	
+  return 0;
+}
